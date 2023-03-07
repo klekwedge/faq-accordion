48fc702b787b8f69f0fe3bad7b4db15dd3bd9192
Andrey <antonov.andrey.10101@gmail.com>
2023-03-07 15:51:36 +0300

    feat: add images

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -14,26 +14,52 @@ import data from '../../data';
 
 function FAQ() {
   return (
-    <Flex p="20px" background="white" borderRadius="10px">
-      {/* <Image/> */}
+    <Flex p="20px" background="white" borderRadius="10px" gap="30px" flexWrap="wrap" justifyContent="center">
+      <Flex position="relative">
+        <Image
+          src="/public/images/illustration-woman-online-desktop.svg"
+          position="relative"
+          w="300px"
+          top="50%"
+          left="-50px"
+          transform="translateY(-50%)"
+        />
+        <Image
+          src="/public/images/bg-pattern-desktop.svg"
+          w="300px"
+          position="absolute"
+          top="55%"
+          left="-50px"
+          transform="translateY(-55%)"
+        />
+        <Image
+          src="/public/images/illustration-box-desktop.svg"
+          w="130px"
+          position="absolute"
+          top="80px"
+          left="-80px"
+        />
+      </Flex>
       <Flex flexDirection="column">
-        <Heading mb="20px" as='h1' fontSize='20px' fontWeight='700'>FAQ</Heading>
+        <Heading mb="20px" as="h1" fontSize="20px" fontWeight="700">
+          FAQ
+        </Heading>
         <Accordion allowToggle>
           {data.map((item) => (
-            <AccordionItem key={uuidv4()} mb="10px" maxW="300px">
-              <Heading mb="5px">
+            <AccordionItem key={uuidv4()} mb="10px" w="270px">
+              <Heading mb="5px" as="h2">
                 <AccordionButton
                   background="transparent"
-                  _expanded={{ color: 'hsl(237, 12%, 33%)' }}
+                  _expanded={{ color: 'hsl(237, 12%, 33%)', fontWeight: 700 }}
                   color="hsl(240, 6%, 50%)"
                 >
-                  <Box as="span" flex="1" textAlign="left">
+                  <Box as="span" flex="1" textAlign="left" fontSize="12px">
                     {item.title}
                   </Box>
                   <AccordionIcon color="#f47b54" />
                 </AccordionButton>
               </Heading>
-              <AccordionPanel>{item.desc}</AccordionPanel>
+              <AccordionPanel fontSize="12px">{item.desc}</AccordionPanel>
             </AccordionItem>
           ))}
         </Accordion>
